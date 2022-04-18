8011545c3a2e457958a783df5d362fc9957be048
sorachung <sorachung.src@gmail.com>
2022-04-18 14:39:34 -0500

    added button for detail view for user profiles

--- Tabloid/client/src/components/userProfiles/UserProfileListItem.js
+++ Tabloid/client/src/components/userProfiles/UserProfileListItem.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const UserProfileListItem = ({ profile }) => {
+  const history = useHistory();
+
   return (
     <tr>
       <td>{profile.displayName}</td>
       <td>{profile.fullName}</td>
       <td>{profile.userType?.name}</td>
 
-      <td></td>
+      <td>
+        <Button
+          onClick={() => history.push(`/userprofiles/details/${profile.id}`)}
+        >
+          Details
+        </Button>
+      </td>
     </tr>
   );
 };
