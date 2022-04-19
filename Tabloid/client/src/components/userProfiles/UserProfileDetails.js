307bc163773597bd7970ae38043b7f7991f8031d
sorachung <sorachung.src@gmail.com>
2022-04-19 09:28:10 -0500

    changed class to className; added image to dir/user details page

--- Tabloid/client/src/components/userProfiles/UserProfileDetails.js
+++ Tabloid/client/src/components/userProfiles/UserProfileDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUserProfile } from "../../modules/UserProfileManager";
+import avatar from "../../images/quill-logo.png";
 
 const UserProfileDetails = () => {
   const [profile, setProfile] = useState({});
@@ -16,31 +17,28 @@ const UserProfileDetails = () => {
   return (
     <section className="container">
       <img
-        src={
-          profile.imageLocation
-            ? profile.imageLocation
-            : "../../../Images/quill-logo.png"
-        }
+        src={profile.imageLocation ? profile.imageLocation : avatar}
         alt="user avatar"
+        width="200em"
       />
       <div>
-        <label class="font-weight-bold">Display Name: </label>
+        <label className="font-weight-bold">Display Name: </label>
         <span> {profile.displayName}</span>
       </div>
       <div>
-        <label class="font-weight-bold">Name: </label>
+        <label className="font-weight-bold">Name: </label>
         <span> {profile.fullName}</span>
       </div>
       <div>
-        <label class="font-weight-bold">Email: </label>
+        <label className="font-weight-bold">Email: </label>
         <span> {profile.email}</span>
       </div>
       <div>
-        <label class="font-weight-bold">Account Creation: </label>
+        <label className="font-weight-bold">Account Creation: </label>
         <span> {profile.createDateTimeFormatted}</span>
       </div>
       <div>
-        <label class="font-weight-bold">User type: </label>
+        <label className="font-weight-bold">User type: </label>
         <span> {profile.userType?.name}</span>
       </div>
       <div></div>
