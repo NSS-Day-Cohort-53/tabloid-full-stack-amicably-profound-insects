d898c72f192cbb84b3cdb8a01d9977702479ed60
sorachung <sorachung.src@gmail.com>
2022-04-19 13:18:27 -0500

    added modal for deactivation confirmation

--- Tabloid/client/src/components/userProfiles/UserProfileListItem.js
+++ Tabloid/client/src/components/userProfiles/UserProfileListItem.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 const UserProfileListItem = ({ profile }) => {
   const history = useHistory();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const deactivate = () => {};
 
   return (
     <tr>
@@ -17,6 +24,50 @@ const UserProfileListItem = ({ profile }) => {
         >
           Details
         </Button>
+        <Button color="danger" onClick={handleShow}>
+          Deactivate
+        </Button>
+        <Modal isOpen={show} onHide={handleClose}>
+          <ModalHeader closeButton>
+            Deactivate {profile.displayName}?
+          </ModalHeader>
+          <ModalBody>
+            <div>
+              <label className="font-weight-bold">Display Name: </label>
+              <span> {profile.displayName}</span>
+            </div>
+            <div>
+              <label className="font-weight-bold">Name: </label>
+              <span> {profile.fullName}</span>
+            </div>
+            <div>
+              <label className="font-weight-bold">Email: </label>
+              <span> {profile.email}</span>
+            </div>
+            <div>
+              <label className="font-weight-bold">Account Creation: </label>
+              <span> {profile.createDateTimeFormatted}</span>
+            </div>
+            <div>
+              <label className="font-weight-bold">User type: </label>
+              <span> {profile.userType?.name}</span>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => {
+                handleClose();
+                deactivate();
+              }}
+            >
+              Deactivate
+            </Button>
+          </ModalFooter>
+        </Modal>
       </td>
     </tr>
   );
